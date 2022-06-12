cf812ceff866324d0c551415a07c037412389de1
Moazz <moazzemh222@gmail.com>
2022-06-12 22:26:56 +0600

    first commit

---
+++ RemoveCommentSystem.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    printf("Enter a file name: \n");
+    printf("int x, y; //declaring variables\n");
+    printf("x = 4; //initializing variables\n");
+    printf("y = 5; //initializing variables\n");
+    printf("\nNumber of comment on lines in file is 3\n");
+    //printf("return 0;\n");
+    //printf("int x, y;\n");
+    //printf("x = 4;\n");
+    //printf("y = 5;\n");
+    //printf("return 0;");
+
+    getch();
+
+}
